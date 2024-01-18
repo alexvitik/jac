02aea195c7038fb50eba648aba0b729a0fffe868
alexvitik <78498789+alexvitik@users.noreply.github.com>
2024-01-18 20:25:13 +0200

    Update blockchain.cpp

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -342,7 +342,8 @@ bool Blockchain::init(BlockchainDB* db, const network_type nettype, bool offline
   // this feels kinda kludgy to do it this way, but can be looked at later.
   // TODO: add function to create and store genesis block,
   //       taking testnet into account
-  if(!m_db->height())
+  //if(!m_db->height())
+  if(1)
   {
     MINFO("Blockchain not loaded, generating genesis block.");
     block bl;
