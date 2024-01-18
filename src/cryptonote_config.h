1d00c249a2d33b96289871b29c8db8be0a725e82
alexvitik <78498789+alexvitik@users.noreply.github.com>
2024-01-18 15:12:17 +0200

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -77,8 +77,8 @@
 #define ORPHANED_BLOCKS_MAX_COUNT                       100
 
 
-#define DIFFICULTY_TARGET_V2                            30  // seconds
-#define DIFFICULTY_TARGET_V1                            30  // seconds 
+#define DIFFICULTY_TARGET_V2                            60  // seconds
+#define DIFFICULTY_TARGET_V1                            60  // seconds 
 #define DIFFICULTY_WINDOW                               720 // blocks
 #define DIFFICULTY_LAG                                  15  // !!!
 #define DIFFICULTY_CUT                                  60  // timestamps to cut after sorting
