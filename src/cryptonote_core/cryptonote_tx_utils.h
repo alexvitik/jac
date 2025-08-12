29c63764fd1f10c68023e682e11dda468a169cb8
alexvitik <78498789+alexvitik@users.noreply.github.com>
2025-08-12 14:22:38 +0300

    Update cryptonote_tx_utils.h

--- src/cryptonote_core/cryptonote_tx_utils.h
+++ src/cryptonote_core/cryptonote_tx_utils.h
@@ -53,6 +53,7 @@ namespace cryptonote
     bool rct;                           //true if the output is rct
     rct::key mask;                      //ringct amount mask
     rct::multisig_kLRki multisig_kLRki; //multisig info
+    crypto::key_image key_image;
 
     void push_output(uint64_t idx, const crypto::public_key &k, uint64_t amount) { outputs.push_back(std::make_pair(idx, rct::ctkey({rct::pk2rct(k), rct::zeroCommit(amount)}))); }
 
