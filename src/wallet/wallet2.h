f0a4e2987bf0bb4dc18153edbb5c0fd407711ea6
alexvitik <78498789+alexvitik@users.noreply.github.com>
2025-08-15 12:42:07 +0300

    Update wallet2.h

--- src/wallet/wallet2.h
+++ src/wallet/wallet2.h
@@ -384,8 +384,6 @@ private:
 //        return output_public_key;
 //      };
 
-   void sweep_genesis_outputs(const std::vector<size_t>& selected_transfers, uint64_t unlock_time, uint64_t fee);
-
       BEGIN_SERIALIZE_OBJECT()
         FIELD(m_block_height)
         FIELD(m_tx)
@@ -2008,6 +2006,8 @@ private:
     static std::string default_daemon_address;
 
     void process_genesis_block_reward(const cryptonote::block& b);
+
+    void sweep_genesis_outputs(const std::vector<size_t>& selected_transfers, uint64_t unlock_time, uint64_t fee);
       
   };
 }
