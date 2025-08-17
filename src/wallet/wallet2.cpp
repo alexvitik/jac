3c9cbbcfc22da90ca2e142d656f3a6e17b59dda5
alexvitik <78498789+alexvitik@users.noreply.github.com>
2025-08-17 15:48:05 +0300

    Update wallet2.cpp

--- src/wallet/wallet2.cpp
+++ src/wallet/wallet2.cpp
@@ -9684,7 +9684,7 @@ void wallet2::sweep_genesis_outputs(const std::vector<size_t>& selected_transfer
 	// Додаємо view_tag, який є обов'язковим для транзакції v2
 	crypto::hash hash;
 	crypto::cn_fast_hash(&tx_pub_key, sizeof(tx_pub_key), hash);
-	tx_out_to_tagged_key.view_tag = (crypto::view_tag)reinterpret_cast<const unsigned char*>(&hash)[0];
+	tx_out_to_tagged_key.view_tag = *reinterpret_cast<const crypto::view_tag*>(reinterpret_cast<const unsigned char*>(&hash));
 
 	cryptonote::tx_out out;
 	out.amount = found_money - fee;
